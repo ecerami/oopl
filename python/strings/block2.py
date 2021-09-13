1aa448e273b34c0dde4245010c9fe098252d27fa
Ethan Cerami <cerami@gmail.com>
2021-09-13 14:48:43 -0400

    Added

---
+++ python/strings/block2.py
@@ -0,0 +1,9 @@
+# Avoid doing this
+msg = "Hello, class!  This is a rather long string. It keeps running on and on.   And goes on even more!\nSuch a long string!"
+
+# Do this instead
+msg = "Hello, class!  This is a rather long string. " \
+    "It keeps running on and on.   And goes on even " \
+    "more!\nSuch a long string!"
+
+print(msg)
