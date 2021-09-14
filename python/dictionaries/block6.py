97a83a0a0730aebedb62ca754c872a6c278eaaa6
Ethan Cerami <cerami@gmail.com>
2021-09-14 10:55:33 -0400

    updated key comments

--- python/dictionaries/block6.py
+++ python/dictionaries/block6.py
@@ -1,4 +1,4 @@
-# Let's see what happens when we try values that don't exist
+# Let's see what happens when we try keys that don't exist
 alien_0 = {"color": "green", "points": 5}
 
 print(alien_0["colour"])
