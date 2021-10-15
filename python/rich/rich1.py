f860460c4970082869a44d3a62267a1de5d3fd26
Ethan Cerami <cerami@gmail.com>
2021-10-15 09:23:33 -0400

    Added

---
+++ python/rich/rich1.py
@@ -0,0 +1,7 @@
+from rich.console import Console
+
+console = Console()
+console.print("Hello World! :thumbs_up:", style="bold red")
+console.print("Where there is a [bold cyan]Will[/bold cyan] there [u]is[/u] " \
+    "a [i]way[/i].")
+
