03e24752897a0479c766968a5ff1f56d2ef18781
Ethan Cerami <cerami@gmail.com>
2022-02-11 15:05:56 -0500

    Added function comment

--- python/functions/block0.py
+++ python/functions/block0.py
@@ -1,6 +1,9 @@
 # Simplest possible function.
 # Note the docstring under greet_user!
 
+# Important:  Make sure you define your function before you call it!
+# Otherwise, Python will fail.
+
 def greet_user():
     """Display a greeting!"""
     print("Hello!")
