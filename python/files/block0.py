e03ed006434786b99c444a812de6604c9bfd7276
Ethan Cerami <cerami@gmail.com>
2022-02-23 16:06:44 -0500

    Fixed file

--- python/files/block0.py
+++ python/files/block0.py
@@ -2,7 +2,5 @@ filename = 'data/pi_digits.txt'
 
 # Open the file and read everything in one big gulp.
 with open(filename) as f:
-    print(type(f))
-    #print(f)
-    #content = f.read()
-#print(content)
+    content = f.read()
+print(content)
