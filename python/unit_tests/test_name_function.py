1ddd0e9017df9bbc4bb71149fb9d9212209d19e5
Ethan Cerami <cerami@gmail.com>
2022-02-26 11:32:07 -0500

    updated

--- python/unit_tests/test_name_function.py
+++ python/unit_tests/test_name_function.py
@@ -5,7 +5,7 @@ class NamesTestCase(unittest.TestCase):
     """Tests for 'name_function.py'."""
     
     def test_first_last_name(self):
-        """Do names like 'Janis Joplin' work?"""
+        """Do names like 'Ethan Cerami' work?"""
         formatted_name = names.get_formatted_name('ethan', 'cerami')
         self.assertEqual(formatted_name, 'Ethan Cerami')
 
