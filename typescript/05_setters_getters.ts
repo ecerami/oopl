15c906a3cfe8aa62923ee6f586820cb9c860bc06
Ethan Cerami <cerami@gmail.com>
2022-04-14 12:31:39 -0400

    Updated example

--- typescript/05_setters_getters.ts
+++ typescript/05_setters_getters.ts
@@ -1,12 +1,12 @@
 class Person {
     public firstName: string;
     public lastName:  string;
-    private _age: number;
+    private _age: number = 0;
 
     constructor(firstName: string, lastName: string, age: number){
         this.firstName = firstName;
         this.lastName = lastName;
-        this._age = age;
+        this.age = age;
     }
 
     get age(): number {
@@ -15,13 +15,13 @@ class Person {
 
     set age(age: number) {
         if (age <= 0 || age >= 200) {
-            throw new Error('The age is invalid');
+            throw new Error('The age is invalid:  ' + age);
         }
         this._age = age;
     }
 }
 
-let person = new Person("John", "Smith", 28)
+let person = new Person("John", "Smith", 200)
 person.age = 29 // This works
 person.age = 250; // This fails
 
