25505c2acb2275e7626fc6eabe35e19b2b4894b0
ecerami <cerami@gmail.com>
2023-09-20 11:57:37 -0400

    Modified comments.

--- python/functions/block3.py
+++ python/functions/block3.py
@@ -1,7 +1,7 @@
 """
 Functions can also return values.
 You can return anything you want from a function.
-e.g. a number, a list, a dict, etc.
+e.g. a number, a list, a tuple, a dictionary, etc.
 """
 
 def add (num1, num2):
