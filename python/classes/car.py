2fb53bac26c31e3f7d7ad838609c9738f51d23c0
ecerami <cerami@gmail.com>
2023-09-22 12:13:40 -0400

    Updated parameters

--- python/classes/car.py
+++ python/classes/car.py
@@ -33,19 +33,19 @@ class Car:
 class ElectricCar(Car):
     """Represent aspects of a car, specific to electric vehicles."""
 
-    def __init__(self, make, model, year):
+    def __init__(self, make, model, year, battery_size):
         """
         Initialize attributes of the parent class.
         Then initialize attributes specific to an electric car.
         """
         super().__init__(make, model, year)
-        self.battery_size = 75
+        self.battery_size = battery_size
 
     def describe_battery(self):
         """Print a statement describing the battery size."""
         print(f"This car has a {self.battery_size}-kWh battery.")
 
 
-my_tesla = ElectricCar("tesla", "model s", 2019)
+my_tesla = ElectricCar("tesla", "model s", 2019, 75)
 print(my_tesla.get_descriptive_name())
 my_tesla.describe_battery()
