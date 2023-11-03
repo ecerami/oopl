10a12f2213ca3b58f695be144fb0d3fdd49cbefe
ecerami <cerami@gmail.com>
2023-11-03 09:45:43 -0400

    Updated for loop

--- javascript/arrays/block2.js
+++ javascript/arrays/block2.js
@@ -9,13 +9,8 @@ console.log(values.indexOf("a"))
 // Note that this returns the first 'b'.
 console.log(values.indexOf("b"))
 
-//  For each example.
-values.forEach(function (item){
-    console.log(item);
-})
-console.log("--------------");
-
-//  For of example.
+//  For example
 for (let item of values) {
     console.log(item);
 }
+
