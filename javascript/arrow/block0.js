850ca5661732b308c6077679b773fa469f81aa97
ecerami <cerami@gmail.com>
2023-11-30 12:05:43 -0500

    Changed character to hero, just to avoid data type confusion.

--- javascript/arrow/block0.js
+++ javascript/arrow/block0.js
@@ -1,11 +1,11 @@
 let marvel = ["thor", "spiderman", "black panther"];
 
 // Arrow Function Example
-let marvelUpperCase = marvel.map(character => {
-	return character.toUpperCase();
+let marvelUpperCase = marvel.map(hero => {
+	return hero.toUpperCase();
 });
 console.log(marvelUpperCase);
 
 // Even More Compact Arrow Function
-marvelUpperCase = marvel.map(character => character.toUpperCase());
+marvelUpperCase = marvel.map(hero => hero.toUpperCase());
 console.log(marvelUpperCase);
